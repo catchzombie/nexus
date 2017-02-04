c960255cf05cfaa1c059dc4b2e98ed8a0cbc259a
Ashish Rathore <ashish.rathore@lendingkart.com>
2017-02-04 20:03:06 +0530

    project structure modified and application.yml for configuration is added

--- nexus-api/src/main/java/com/catchzombie/api/ApplicationMain.java
+++ nexus-api/src/main/java/com/catchzombie/api/ApplicationMain.java
@@ -1,14 +1,22 @@
 package com.catchzombie.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Created by ashsish on 2/2/17.
  */
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.catchzombie")
+@EntityScan("com.catchzombie.models")
+@EnableJpaRepositories("com.catchzombie.repository")
 public class ApplicationMain extends SpringBootServletInitializer {
 
     @Override
