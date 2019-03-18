c9b3b5ea5236b07735dcfb1eb38f087abbe38b43
Shubham Srivastava <shubhamsrivastava@Shubhams-MacBook-Air.local>
2019-03-18 10:55:19 +0530

    updates

--- nexus-api/src/main/java/com/catchzombie/api/ApplicationMain.java
+++ nexus-api/src/main/java/com/catchzombie/api/ApplicationMain.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
